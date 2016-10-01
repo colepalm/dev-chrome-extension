686cec50dc740729616ed76115e85e9c0a3bfc60
Cole Palm <palm.cole@gmail.com>
2016-10-01 16:19:43 -0700

    mostly copy

---
+++ background.js
@@ -0,0 +1,20 @@
+var _gaq = _gaq || [];
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+chrome.commands.onCommand.addListener(function(command) {
+
+  chrome.storage.local.get("definedURL", function(result){
+    if(result.definedURL!=undefined){
+      chrome.tabs.update({url:'http://'+result.definedURL});
+      _gaq.push(['_trackEvent', 'refresh', result.definedURL]);
+    }else{
+      chrome.tabs.update({url:'http://i.imgur.com/Jl1wSJi.jpg'});
+    }
+  });
+
+});
